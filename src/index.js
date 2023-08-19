313539a264981d4f761ccb23d284d123382d234e
evikary <daria_more10@mail.ru>
2023-08-19 23:29:13 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -5,19 +5,35 @@ function towelSort(matrix) {
     if (!matrix) {
         return res;
     }
-    matrix.forEach((item, index) => {
-        if (index % 2 === 0) {
-            for (let i = 0; i <= item.length - 1; i++) {
-                res.push(item[i]);
-            }
-        } else {
-            for (let i = item.length - 1; i >= 0; i--) {
-                res.push(item[i]);
-            }
-        }
+
+    matrix.map((item, index) => {
+        index % 2 === 0 ? res.push(...item) : res.push(...item.reverse());
     });
 
     return res;
 }
+
+//solved by a cycle
+
+// function towelSort(matrix) {
+//     let res = [];
+//     if (!matrix) {
+//         return res;
+//     }
+
+//     for (let i = 0; i <= matrix.length - 1; i++) {
+//         if (i % 2 === 0) {
+//             for (let j = 0; j <= matrix[i].length - 1; j++) {
+//                 res.push(matrix[i][j]);
+//             }
+//         } else {
+//             for (let j = matrix[i].length - 1; j >= 0; j--) {
+//                 res.push(matrix[i][j]);
+//             }
+//         }
+//     }
+
+//     return res;
+// }
 
 module.exports = towelSort;
